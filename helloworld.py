75d8254783ae9ccca5c0e19973b2052521696747
sky <skywalker878@gmail.com>
2023-09-19 21:34:45 -0700

    print diamond shape

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,15 @@
+for i in range(0,4):
+    for j in range(0,4-i):
+        print(" ", end="")
+    for k in range(0,2*i+1):
+        print("*",end="")
+    print()
+
 for i in range(0,5):
     for j in range(0,i):
         print(" ",end="")
     for k in range(0,(4-i)*2 + 1):
         print("*", end="")
     print()
-    
+   
 
