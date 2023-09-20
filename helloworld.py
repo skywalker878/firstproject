d2678f7553aba37f77eb9dd3b00d0cca95ed9da7
sky <skywalker878@gmail.com>
2023-09-19 21:29:32 -0700

    second modified

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,8 @@
-print("Hello World!!")
+for i in range(0,5):
+    for j in range(0,i):
+        print(" ",end="")
+    for k in range(0,(4-i)*2 + 1):
+        print("*", end="")
+    print()
+    
 
